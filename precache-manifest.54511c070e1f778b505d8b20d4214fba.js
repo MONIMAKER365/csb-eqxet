eb0a5f6e662b3737c2d68f9449a5221df870fcaa
Code Sandbox <hello@codesandbox.io>
2021-05-25 19:37:40 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.54511c070e1f778b505d8b20d4214fba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60966eed6caaa0fd65753583a074c634",
+    "url": "https://monimaker365.github.io/csb-eqxet/index.html"
+  },
+  {
+    "revision": "ff2c0d46d74d3cc0958c",
+    "url": "https://monimaker365.github.io/csb-eqxet/static/css/main.93fa5b95.chunk.css"
+  },
+  {
+    "revision": "e9b9eae2fcf5472e5185",
+    "url": "https://monimaker365.github.io/csb-eqxet/static/js/2.e25e61aa.chunk.js"
+  },
+  {
+    "revision": "ff2c0d46d74d3cc0958c",
+    "url": "https://monimaker365.github.io/csb-eqxet/static/js/main.9884436c.chunk.js"
+  },
+  {
+    "revision": "34f594a8700964f31a3e",
+    "url": "https://monimaker365.github.io/csb-eqxet/static/js/runtime~main.0942d927.js"
+  }
+]);
